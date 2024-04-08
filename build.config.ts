21f1ef7982e5f92d46ba06cfd940170e63c1c844
xuewuding <dingxw@sxj.com>
2024-04-08 17:42:58 +0800

    first commit

---
+++ build.config.ts
@@ -0,0 +1,14 @@
+import { defineBuildConfig } from "unbuild";
+
+export default defineBuildConfig({
+  entries: ["src/index"],
+  clean: true,
+  rollup: {
+    inlineDependencies: true,
+    esbuild: {
+      target: "ES6",
+      minify: true,
+    },
+  },
+  declaration: true,
+});
